b4c3a745a705256dbea164627aeac76dda3cbea2
xentixar <xentixar@gmail.com>
2025-03-31 19:52:31 +0545

    Update button labels and styles in the add comment form

--- resources/views/livewire/add-comment.blade.php
+++ resources/views/livewire/add-comment.blade.php
@@ -3,14 +3,14 @@
         <form class="mt-6 space-y-4" wire:submit="create">
             {{ $this->form }}
             <x-filament::button type="submit">
-                Reply
+                Add Comment
             </x-filament::button>
-            <x-filament::button color="danger" type="button" wire:click="$set('replyingTo', null)">
+            <x-filament::button color="danger" type="button" wire:click="$set('isOpen', false)">
                 Cancel
             </x-filament::button>
         </form>
     @else
-        <x-filament::button wire:click="$set('isOpen', true)" class="mt-6">
+        <x-filament::button outlined wire:click="$set('isOpen', true)" class="mt-6">
             {{ __('Add a comment') }}
         </x-filament::button>
     @endif
